15d62d007ef3ac93a6fa2a9063ab007e8067c94e
Zahra Hassani <raha.zahra1375@gmail.com>
2023-02-22 09:24:59 +0000

    Update admin files

--- poster/admin.py
+++ poster/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['name']
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'address', 'status', 'created_on')
+    search_fields = ['title', 'description', 'color', 'cost']
+    list_filter = ('status', 'approved', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+    actions = ['approve_post']
+
+    def approve_post(self, request, queryset):
+        queryset.update(approved=True)
+
+
--- poster/views.py
+++ poster/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=0).filter(
+        approved=True).order_by('-cleated_on')
+    template_name = 'index.html'
+    paginate_by = 6
