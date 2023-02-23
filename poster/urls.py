9a93076775e65bea7fb2467c9aa563d2042da007
Zahra Hassani <raha.zahra1375@gmail.com>
2023-02-23 16:03:30 +0000

    Add update post

--- poster/urls.py
+++ poster/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("posts/my-posts", views.MyPostList.as_view(), name="my_posts"),
     path("post/new", views.NewPost.as_view(), name="new_post"),
+    path('post/edit/<slug:slug>/', views.EditPost.as_view(),
+         name='post_update'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- poster/views.py
+++ poster/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.views import generic, View
+from django.urls import reverse_lazy
 from .models import Post, Category
 from .forms import CategoryForm, PostForm
 from django.template.defaultfilters import slugify
@@ -83,6 +86,50 @@ class NewPost(View):
                 },
             )
 
+
+class EditPost(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug) 
+        post_form = PostForm(instance=post)
+        return render(
+            request,
+            "edit_post.html",
+            {
+                "post_form": post_form
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug) 
+        post_form = PostForm(data=request.POST, files=request.FILES, instance=post)
+
+        if post_form.is_valid():
+            epost = post_form.save(commit=False)
+            epost.slug = slugify(post_form.cleaned_data['title'])
+            epost.save()
+            category = epost.category
+            return render(
+                request,
+                "post_detail.html",
+                {
+                    "post": epost,
+                    "category": category,
+                },
+            )
+        else:
+            post_form = PostForm()
+            return render(
+                request,
+                "new_post.html",
+                {
+                    "post_form": PostForm
+                },
+            )
+
+    
+   
 
 class CategoryList(generic.ListView):
     model = Category
